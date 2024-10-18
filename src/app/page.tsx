326815b9e8f9bf62f68acb8902172584ad27f4da
Ethan Colinet <126577076+Arhanys@users.noreply.github.com>
2024-10-18 17:55:59 +0200

    Created a test component and cleaned up the tailwind file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import TestComponent from "@/components/testComponent";
+
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <>
+      <div>
+        <h1>Home</h1>
+      </div>
+      <TestComponent />
+    </>
   );
 }
---
+++ src/components/testComponent.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TestComponent: React.FC = () => {
+  return (
+    <div>
+      <h1 className="bg-red-500 w-fit mx-auto">Hello, this is a test component!</h1>
+    </div>
+  );
+};
+
+export default TestComponent;
