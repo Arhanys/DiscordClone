e78be4dc348adbb4c4bccb04dab12de990f2cb9c
Florian Donckers <fdonckersf@gmail.com>
2024-10-23 03:02:10 +0200

    Reorganized Api folder

--- src/app/addUser/page.tsx
+++ src/app/addUser/page.tsx
@@ -1,33 +1,48 @@
-import addUser from '../../Database/addUser';
+import addUser from '../../api/database/addUser';
 import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
-export const dynamic = "force-dynamic";
+export const dynamic = 'force-dynamic';
 
 export default async function addUserScreen() {
-    const users = await prisma.user.findMany();
+	const users = await prisma.user.findMany();
 
-    return (
-        <div className=''>
-            <form action={addUser} className='m-2  flex gap-5'>
-                <input type="text" name='pseudo' placeholder='votre pseudo' className='border border-black rounded-xl text-center'/>
-                <input type="email" name='email' placeholder='votre email' className='border border-black rounded-xl text-center'/>
-                <input type="text" name='pass' placeholder='votre mot de passe' className='border border-black rounded-xl text-center'/>
-                <button type='submit' className='bg-slate-200 rounded-xl text-center'>Add user</button>
-            </form>
+	return (
+		<div className="">
+			<form action={addUser} className="m-2  flex gap-5">
+				<input
+					type="text"
+					name="pseudo"
+					placeholder="votre pseudo"
+					className="border border-black rounded-xl text-center"
+				/>
+				<input
+					type="email"
+					name="email"
+					placeholder="votre email"
+					className="border border-black rounded-xl text-center"
+				/>
+				<input
+					type="text"
+					name="pass"
+					placeholder="votre mot de passe"
+					className="border border-black rounded-xl text-center"
+				/>
+				<button type="submit" className="bg-slate-200 rounded-xl text-center">
+					Add user
+				</button>
+			</form>
 
-            <ul>
-                {
-                    users.map((user) => (
-                        <li key={user.id} className='flex flex-col m-5 border max-w-fit'>
-                            <span>pseudo: {user.pseudo}</span>
-                            <span>email: {user.email}</span>
-                            <span>password: {user.password}</span>
-                            <span>id: {user.id}</span>
-                        </li>
-                    ))
-                }
-            </ul>
-        </div>
-    )
+			<ul>
+				{users.map((user) => (
+					<li key={user.id} className="flex flex-col m-5 border max-w-fit">
+						<span>pseudo: {user.pseudo}</span>
+						<span>email: {user.email}</span>
+						<span>password: {user.password}</span>
+						<span>id: {user.id}</span>
+					</li>
+				))}
+			</ul>
+		</div>
+	);
 }
